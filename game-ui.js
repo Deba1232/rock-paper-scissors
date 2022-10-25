84148b4d8b3d546c074d0729b749e5dcb5737d12
DeV <debanjan.das90@gmail.com>
2022-10-25 22:27:06 +0530

    new commit

---
+++ game-ui.js
@@ -0,0 +1,47 @@
+let selector2 = document.querySelector('.warrior-list');
+let resultWindow = document.querySelector('.result-window');
+
+let playerChoiceResult = resultWindow.querySelector('.user-choice');
+let opponentChoiceResult = resultWindow.querySelector('.opponent-choice');
+
+selector2.addEventListener('click', function(e){
+    // document.location.reload();
+    if (e.target.children.length < 3){
+
+        if (resultWindow.querySelector('.user-choice img') !== null) {
+            playerChoiceResult.removeChild(playerChoiceResult.firstElementChild);
+
+            if (e.target.nodeName != 'DIV') {
+
+                let childElements = e.target.parentElement.children;
+                playerChoiceResult.insertAdjacentElement('beforeend', childElements[0].cloneNode());
+
+            }
+
+            else {
+                let childElements = e.target.children;
+                playerChoiceResult.insertAdjacentElement('beforeend', childElements[0].cloneNode());
+            }
+        }
+
+        else {
+
+            if (e.target.nodeName != 'DIV') {
+
+                let childElements = e.target.parentElement.children;
+                playerChoiceResult.insertAdjacentElement('beforeend', childElements[0].cloneNode());
+
+            }
+
+            else {
+                let childElements = e.target.children;
+                playerChoiceResult.insertAdjacentElement('beforeend', childElements[0].cloneNode());
+            }
+
+        }
+
+    }
+    
+
+    
+});
---
+++ game.js
@@ -0,0 +1,132 @@
+let game = (userChoice, opponentChoice) => {
+    
+    if(userChoice === opponentChoice)
+        return 'Draw';
+
+    else if (userChoice === 'ROCK' && opponentChoice === 'PAPER')
+        return 'You lost';
+
+    else if (userChoice === 'ROCK' && opponentChoice === 'SCISSORS')
+        return 'You win';
+
+    else if (userChoice === 'PAPER' && opponentChoice === 'ROCK')
+        return 'You win';
+
+    else if (userChoice === 'PAPER' && opponentChoice === 'SCISSORS')
+        return 'You lost';
+
+    else if (userChoice === 'SCISSORS' && opponentChoice === 'ROCK')
+        return 'You lost';
+
+    else if (userChoice === 'SCISSORS' && opponentChoice === 'PAPER')
+        return 'You win';
+
+    else
+        return 'You made an invalid choice';
+};
+
+const opponentOptions = ['Rock','Paper','Scissors'];
+
+let randomChoice; 
+let userChoice;
+let opponentChoice;
+
+let playerPoints = 0;
+let opponentPoints = 0;
+
+let playCount = 0;
+
+let selector = document.querySelector('.warrior-list');
+
+selector.addEventListener('click', function(e) {
+
+    if (e.target.children.length < 3){
+
+        if (e.target.alt !== undefined) {
+            userChoice = e.target.alt;
+        }
+
+        else {
+            userChoice = e.target.textContent.replace(/\s/g, '');
+        }
+
+        console.log(userChoice);
+
+        randomChoice = Math.floor(Math.random() * opponentOptions.length);
+        opponentChoice = opponentOptions[randomChoice];
+
+        let result = game(userChoice.toUpperCase(), opponentChoice.toUpperCase());
+
+        console.log(opponentChoice, result);
+
+        if (result === 'You win') {
+            playerPoints++;
+        }
+
+        else if (result === 'You lost') {
+            opponentPoints++;
+        }
+
+        else {
+            console.log('No points added');
+        }
+
+
+        console.log(`Your Score : ${playerPoints}\nOpponent's score : ${opponentPoints}`);
+
+    }
+    
+
+});
+
+
+function result(playerPoints, opponentPoints){
+    if (playerPoints > opponentPoints)
+        console.log("You win!");
+
+    else if (playerPoints < opponentPoints)
+        console.log("You lost!");
+
+    else
+        console.log("It's a draw!");
+}
+
+
+
+
+
+
+
+
+/* for (i=0;i<5;i++){
+    
+    
+    console.log(userChoice);
+
+    randomChoice = Math.floor(Math.random() * opponentOptions.length);
+    opponentChoice = opponentOptions[randomChoice];
+
+    let result = game(userChoice.toUpperCase(), opponentChoice.toUpperCase());
+
+    // console.log(opponentChoice,result);
+
+    if (result === 'You win')
+        playerPoints++;
+
+    else if (result === 'You lost')
+        opponentPoints++;
+
+    else
+        console.log('No points added');
+}
+
+console.log(`Your Score : ${playerPoints}\nOpponent's score : ${opponentPoints}`);
+
+if (playerPoints > opponentPoints)
+    console.log("You win!");
+
+else if (playerPoints < opponentPoints)
+    console.log("You lost!");
+
+else
+    console.log("It's a draw!"); */
